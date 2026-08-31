7c4b52e46cfcd6cb89f46c8efc2be6ba4a4892dd
THAITECHZONE <thaitechzone@users.noreply.github.com>
2026-08-31 13:47:40 +0700

    Update Cpp main

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include <Arduino.h>
  
-
+int statusLed = 2;
 void setup() {
+    pinMode(statusLed, OUTPUT);
   
 }
 
 void loop() {
-   
+    digitalWrite(statusLed, HIGH);
+    delay(100);
+    digitalWrite(statusLed, LOW);
+    delay(1000);
 }
 
  
